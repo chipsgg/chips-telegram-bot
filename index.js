0027f8ea29a31ac91b53b6aa516399c908019f40
aabbdev <luc.marie@protonmail.com>
2021-08-30 15:17:39 +0000

    fix in the design

--- index.js
+++ index.js
@@ -25,16 +25,12 @@ API.init()
     })
     bot.command('events', async ctx => {
       const activeRaces = await API.listActiveRaces(skip = 0, limit = 10)
-      if (activeRaces.length > 0) {
-        bot.telegram.sendMessage(ctx.chat.id, models.events(activeRaces), { parse_mode: "HTML", disable_web_page_preview: true })
-      } else {
-        bot.telegram.sendMessage(ctx.chat.id, "There are no races at the moment!", {})
-      }
+      bot.telegram.sendMessage(ctx.chat.id, models.events(activeRaces), { parse_mode: "HTML", disable_web_page_preview: true })
     })
     bot.command('prices', ctx => {
       const prices = API.get('prices')
       const currencies = API.get('currencies')
-
+      // preprocessing
       const pairs = _.reduce(_.keys(prices), (output, current) => {
         if (current.endsWith("USDT")) {
           const pair1 = _.first(current.split('USDT', 1))
@@ -61,6 +57,7 @@ API.init()
         return output
       }, [])
       const pairsFiltered = _.filter(pairs, (item) => currencies.hasOwnProperty(_.toLower(item.pair1)))
+  
       bot.telegram.sendMessage(ctx.chat.id, models.prices(pairsFiltered), { parse_mode: "HTML" })
     })
     bot.command('groups', ctx => {
--- models/events/scheme.js
+++ models/events/scheme.js
@@ -3,19 +3,21 @@ const _ = require('lodash')
 const { formatDate } = require('../../utils')
 module.exports = (events) => {
   assert(events, "requires events")
-  assert(events.length > 0, "requires at least one event")
-
-  const content = _.chain(events)
-    .map(({ title, endTime, subtitle }) => (`---------
+  if (events.length > 0) {
+    const content = _.chain(events)
+      .map(({ title, endTime, subtitle }) => (`---------
 <strong>${title}</strong>
 <strong>Ends:</strong> ${formatDate(endTime)}
 
 <em>${subtitle}</em>`))
-    .join("\n")
-    .value()
-  // { title, endTime, subtitle }
-  return `<strong>ONGOING EVENTS</strong>\n${content}
+      .join("\n")
+      .value()
+    // { title, endTime, subtitle }
+    return `<strong>ONGOING EVENTS</strong>\n${content}
 
 ---------
 <a href="https://chips.gg/events">Click here to find out more about our events!</a>`
+  } else {
+    return "There are no races at the moment!"
+  }
 }
