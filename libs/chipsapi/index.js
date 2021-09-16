a0272a7d4348fbe64a11181f2fe2a057875dd3b9
aabbdev <luc.marie@protonmail.com>
2021-09-16 16:41:24 +0000

    v2.3.1

--- libs/chipsapi/index.js
+++ libs/chipsapi/index.js
@@ -1,38 +1,37 @@
 const WS = require('ws');
-const Client = require('@chipsgg/openservice-ws-client');
+const Client = require('@chipsgg/ws-client');
 const _ = require('lodash')
 
-module.exports = (isbeta) => {
-  if (isbeta) host = 'wss://staging.chips.gg';
-  else host = 'wss://api.chips.gg/prod/socket';
-  wsclient = null
-  state = {}
+module.exports = () => {
+  let api = null
+  let state = {}
   return {
     async init() {
-      wsclient = await Client(WS, {
-        channels: [
-          'games',
-          'public',
-          'private',
-          'auth',
-          'affiliates',
-          'stats',
-          'profitshare',
-        ], host
-      }, (type, newState) => {
-        if (type === "change") {
-          state = {
-            ...state,
-            ...newState
+      api = await Client(WS, [
+        'games',
+        'public',
+        'private',
+        'auth',
+        'affiliates',
+        'stats',
+        'profitshare',
+      ], (type, newState) => {
+        switch (type) {
+          case "change": {
+            state = {
+              ...state,
+              ...newState
+            }
+            break;
           }
         }
       })
-      await wsclient.actions.profitshare('on', { name: "profitshareBalance" })
-      await wsclient.actions.profitshare('on', { name: "profitshareInfo" })
+      await api.actions.profitshare('on', { name: "profitshareBalance" })
+      await api.actions.profitshare('on', { name: "profitshareInfo" })
     },
     state: () => state,
     get: (...path) => _.get(state, path),
-    listRaceRanks: (raceid) => wsclient.actions.public('listRaceRanks', { raceid }),
-    listActiveRaces: (skip = 0, limit = 10) => wsclient.actions.public('listActiveRaces', { skip, limit })
+    listRaceRanks: (raceid) => api.actions.public('listRaceRanks', { raceid }),
+    listActiveRaces: (skip = 0, limit = 10) => api.actions.public('listActiveRaces', { skip, limit })
   }
 }
