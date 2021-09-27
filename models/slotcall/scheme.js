1923c48b3c32a0cf656f2f6177ddfd12a56a068d
aabbdev <luc.marie@protonmail.com>
2021-09-27 16:41:56 +0000

    fix infinite numbers

--- models/slotcall/scheme.js
+++ models/slotcall/scheme.js
@@ -1,12 +1,13 @@
 const assert = require('assert')
 const _ = require('lodash')
+const Humanize = require('humanize-plus')
 
 module.exports = ({name, product, rtp, url_thumb}) => {
   return `
 🎰 <strong>Chips Casino Slot Call</strong> 🎰 
 🎩 ${name} 
 🎩 ${product}
-🎩 RTP ${rtp}% 
+🎩 RTP ${Humanize.formatNumber(rtp,2)}% 
   
 Good Luck and may the Chips be forever stacked in your favour ⭐️`
 }
