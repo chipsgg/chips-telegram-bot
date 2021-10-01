18b511269c4c8c3a06e36fd19d4ce4c9e03670fc
aabbdev <luc.marie@protonmail.com>
2021-10-01 16:10:43 +0000

    revert config

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ const superAdmins = [
   "aabbdev"
 ]
 
-const mainGroup = -599487309//-1001380278636; // production
+const mainGroup = -1001380278636; // production
 
 module.exports = {
   ambassadors,
