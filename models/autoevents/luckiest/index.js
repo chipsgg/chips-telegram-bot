55382e6533c2ab163a1a6bed192092ba900b53be
aabbdev <luc.marie@protonmail.com>
2021-10-11 13:52:05 +0000

    big fix/visual enhancements

--- models/autoevents/luckiest/index.js
+++ models/autoevents/luckiest/index.js
@@ -3,11 +3,13 @@ const Humanize = require('humanize-plus')
 const { formatCurrency } = require('@coingecko/cryptoformat')
 
 module.exports = ({ slotname, multiplier, amount, currency, currencyInfo, winnings }) => {
+  const amountValue = parseFloat(Humanize.formatNumber(amount / Math.pow(10, currencyInfo.decimals), currencyInfo.decimals))
+  const winningsValue = parseFloat(Humanize.formatNumber(winnings / Math.pow(10, currencyInfo.decimals), currencyInfo.decimals))
   return `🍀🍀 <strong>Lucky Win Alert</strong> 🍀🍀
 
-🕹 ${slotname}
-🪙 ${amount / Math.pow(10, currencyInfo.decimals)} ${_.upperCase(currency)}
-💰 ${formatCurrency((winnings / Math.pow(10, currencyInfo.decimals)) * currencyInfo.price, "USD", "en")}
+🕹️ ${slotname}
+🤞 ${amountValue} ${_.upperCase(currency)} (${amountValue * currencyInfo.price})
+💰 ${formatCurrency(winningsValue * currencyInfo.price, "USD", "en")}
 
 ✖️ <strong>${Humanize.formatNumber(multiplier, 2)}x Multiplier</strong>
 
