fb006ee3fdfbb5ee4f6b6ae0a7460120bcf25196
Jacob D. <jdent@tacyarg.com>
2024-12-20 12:12:58 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const MarkdownIt = require("markdown-it");
 const md = new MarkdownIt({
   highlight: function (str, lang) {
     return `<pre class="code-block"><code class="language-${lang}">${md.utils.escapeHtml(str)}</code></pre>`;
-  }
+  },
 });
 
 app.get("/", (req, res) => {
@@ -78,18 +78,18 @@ app.get("/commands", (req, res) => {
     }
   });
 
-  // if (process.env.DISCORD_TOKEN) {
-  //   connectors.push(await Discord(process.env.DISCORD_TOKEN, commands));
-  // }
+  if (process.env.DISCORD_TOKEN) {
+    connectors.push(await Discord(process.env.DISCORD_TOKEN, commands));
+  }
 
-  // if (process.env.TELEGRAM_TOKEN) {
-  //   try {
-  //     const telegram = await Telegram(process.env.TELEGRAM_TOKEN, commands);
-  //     connectors.push(telegram);
-  //   } catch (error) {
-  //     console.error("Error starting Telegram bot:", error);
-  //   }
-  // }
+  if (process.env.TELEGRAM_TOKEN) {
+    try {
+      const telegram = await Telegram(process.env.TELEGRAM_TOKEN, commands);
+      connectors.push(telegram);
+    } catch (error) {
+      console.error("Error starting Telegram bot:", error);
+    }
+  }
 
   const broadcastText = makeBroadcast(connectors, "broadcastText");
   const broadcastForm = makeBroadcast(connectors, "broadcastForm");
