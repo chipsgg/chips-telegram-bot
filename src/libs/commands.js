c910e1aae1c0ee7d9c9c038047f6be5fd5a261d8
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-10 17:24:55 -0400

    Send image response for `/compare` in Discord

--- src/libs/commands.js
+++ src/libs/commands.js
@@ -621,26 +621,62 @@ module.exports = (api) => {
     },
     handler: async (ctx) => {
       let username1, username2;
-      if (ctx.platform === "discord") {
-        username1 = ctx?.getString("username1");
-        username2 = ctx?.getString("username2");
-        if (!username1 || !username2) {
-          return ctx.sendText("Please provide both usernames to compare");
-        }
-      } else {
+      if (ctx.platform !== "discord") {
         username1 = ctx?.getArg(1);
         username2 = ctx?.getArg(2);
         if (!username1 || !username2) {
           return ctx.sendText("Please provide both usernames to compare");
         }
+
+        return ctx.sendForm({
+          emoji: "🔄",
+          title: `Comparing ${username1} vs ${username2}`,
+          banner: `https://stats.chips.gg/compare/${username1}/${username2}`,
+          buttonLabel: "View Profiles",
+          url: `https://chips.gg/user/${username1}`,
+        });
       }
 
-      return ctx.sendForm({
-        emoji: "🔄",
-        title: `Comparing ${username1} vs ${username2}`,
-        banner: `https://stats.chips.gg/compare/${username1}/${username2}`,
-        buttonLabel: "View Profiles",
-        url: `https://chips.gg/user/${username1}`,
+      username1 = ctx?.getString("username1");
+      username2 = ctx?.getString("username2");
+      if (!username1 || !username2) {
+        return ctx.sendText("Please provide both usernames to compare");
+      }
+
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setStyle(ButtonStyle.Link)
+          .setLabel(`View ${username1.slice(0, 19)}`)
+          .setURL("https://chips.gg/user/" + username1),
+        new ButtonBuilder()
+          .setStyle(ButtonStyle.Link)
+          .setLabel(`View ${username2.slice(0, 19)}`)
+          .setURL("https://chips.gg/user/" + username2)
+      );
+
+      const image = await fetch(
+        `https://stats.chips.gg/compare/${username1}/${username2}`
+      );
+      if (
+        !image.ok ||
+        !image.headers.get("content-type").startsWith("image/")
+      ) {
+        await ctx.interaction.deleteReply();
+        await ctx.interaction.followUp({
+          content: `Failed to fetch comparison image! Please try again later.`,
+          flags: [MessageFlags.Ephemeral],
+        });
+        return;
+      }
+
+      const buffer = await image.arrayBuffer();
+      await ctx.interaction.editReply({
+        files: [
+          new AttachmentBuilder(Buffer.from(buffer), {
+            name: "comparison.png",
+          }),
+        ],
+        components: [row],
       });
     },
   };
