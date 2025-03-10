f60f12bdfe193f57539a448e3f65edbcde9fbb80
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-10 18:00:51 -0400

    Add command loader to scan files in `src/commands`

--- src/libs/commands.js
+++ src/libs/commands.js
@@ -661,5 +661,33 @@ module.exports = (api) => {
     handler: (ctx) => ctx.sendForm(models.help(commands)),
   };
 
+  const loadedCommands = loadCommands(api);
+  Object.assign(commands, loadedCommands);
+
+  console.log(`Loaded ${Object.keys(commands).length} commands!`);
+
   return commands;
 };
+
+function loadCommands(api) {
+  const fs = require("fs");
+  const path = require("path");
+
+  const commands = {};
+  const dir = path.join(__dirname, "../commands");
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    if (!file.endsWith(".js")) continue;
+
+    try {
+      const module = require(path.join(dir, file));
+      const command = module(api);
+      commands[command.name || file.replace(".js", "").trim()] = command;
+    } catch (error) {
+      console.error("Error loading command:", file, error);
+    }
+  }
+
+  return commands;
+}
