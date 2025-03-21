380ebf03c9ba54ad335863f41dc07bb7249acad9
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-20 22:01:32 -0400

    Move `/stats`

--- src/libs/commands.js
+++ src/libs/commands.js
@@ -1,14 +1,7 @@
 const assert = require("assert");
 const _ = require("lodash");
 const models = require("./models");
-const {
-  ApplicationCommandOptionType,
-  MessageFlags,
-  AttachmentBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-} = require("discord.js");
+const { ApplicationCommandOptionType, MessageFlags } = require("discord.js");
 
 module.exports = (api) => {
   assert(api, "requires api");
@@ -443,55 +436,6 @@ module.exports = (api) => {
       } catch (error) {
         return ctx.sendText("Error searching games: " + error.message);
       }
-    },
-  };
-
-  commands.stats = {
-    description: "Show user stats banner. Usage: /stats username",
-    options: {
-      username: {
-        description: "Chips.gg username",
-        type: ApplicationCommandOptionType.String,
-        required: true,
-      },
-    },
-    handler: async (ctx) => {
-      let username = null;
-      if (ctx.platform !== "discord") {
-        username = ctx?.getArg(1);
-        if (!username) {
-          return ctx.sendText("Please provide a username");
-        }
-        return ctx.sendForm({
-          emoji: "📊",
-          title: `Stats Banner: ${username}`,
-          // content: "Here are your stats:",
-          banner: `https://stats.chips.gg/stats/${username}`,
-          buttonLabel: "View Profile",
-          url: `https://chips.gg/user/${username}`,
-        });
-      }
-
-      username = ctx?.getString("username");
-      if (!username) {
-        return ctx.sendText("Please provide a username");
-      }
-
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setStyle(ButtonStyle.Link)
-          .setLabel(`View ${username.slice(0, 19)}`)
-          .setURL("https://chips.gg/user/" + username)
-      );
-
-      await ctx.interaction.editReply({
-        files: [
-          new AttachmentBuilder("https://stats.chips.gg/stats/" + username, {
-            name: `${username}.png`,
-          }),
-        ],
-        components: [row],
-      });
     },
   };
 
