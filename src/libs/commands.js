18ae6d88c91f4161239454da5ac50894771032cc
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-20 22:08:28 -0400

    Move `/chat`

--- src/libs/commands.js
+++ src/libs/commands.js
@@ -8,23 +8,6 @@ module.exports = (api) => {
 
   const commands = {};
 
-  commands.chat = {
-    description: "Get invite links to official Telegram/Discord communities",
-    handler: (ctx) =>
-      ctx.sendForm({
-        emoji: "💬",
-        title: "Official Communities",
-        content: [
-          "Join our communities to chat with other players!",
-          "",
-          "Discord: https://discord.gg/chips",
-          "Telegram: https://t.me/chipsgg",
-        ].join("\n"),
-        buttonLabel: "Join Discord",
-        url: "https://discord.gg/chips",
-      }),
-  };
-
   commands.mostplayed = {
     description: "List most played games",
     handler: async (ctx) => {
