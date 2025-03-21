4823d11a7589f1461bd732dcc4d8e4c2b067367b
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-20 21:52:15 -0400

    Move `/bet`

--- src/libs/commands.js
+++ src/libs/commands.js
@@ -614,39 +614,6 @@ module.exports = (api) => {
     },
   };
 
-  commands.bet = {
-    description: "Show bet card. Usage: /bet betid",
-    options: {
-      betid: {
-        description: "Bet ID",
-        type: ApplicationCommandOptionType.String,
-        required: true,
-      },
-    },
-    handler: async (ctx) => {
-      let betId = null;
-      if (ctx.platform === "discord") {
-        betId = ctx?.getString("betid");
-        if (!betId) {
-          return ctx.sendText("Please provide a bet ID");
-        }
-      } else {
-        betId = ctx?.getArg(1);
-        if (!betId) {
-          return ctx.sendText("Please provide a bet ID");
-        }
-      }
-
-      return ctx.sendForm({
-        emoji: "🎲",
-        title: `Bet: ${betId}`,
-        banner: `https://stats.chips.gg/bets/${betId}`,
-        // buttonLabel: "View Bet",
-        // url: `https://chips.gg/bets/${betId}`,
-      });
-    },
-  };
-
   commands.help = {
     description: "Description of all commands",
     handler: (ctx) => ctx.sendForm(models.help(commands)),
