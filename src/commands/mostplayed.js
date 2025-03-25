33cdf9514e48ae49bdca1cfe191c7dc7e737397c
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-24 23:37:09 -0400

    Add hyperlinks to `/mostplayed`

--- src/commands/mostplayed.js
+++ src/commands/mostplayed.js
@@ -8,14 +8,25 @@ module.exports = (api) => ({
       duration: "1m",
     });
 
+    const content =
+      ctx.platform === "discord" || ctx.platform === "telegram"
+        ? games.map(hyperlinkMap).join("\n")
+        : games.map(defaultMap).join("\n");
+
     return ctx.sendForm({
       emoji: "🎮",
       title: "Most Played Games",
-      content: games
-        .map((game, index) => `${index + 1}. ${game.title} (${game.provider})`)
-        .join("\n"),
+      content: content,
       url: "https://chips.gg/casino",
       buttonLabel: "Play Now",
     });
   },
 });
+
+function hyperlinkMap(game, index) {
+  return `${index + 1}. **[${game.title}](https://chips.gg/play/${game.slug})** by [${game.provider}](https://chips.gg/casino/providers/${game.provider})`;
+}
+
+function defaultMap(game, index) {
+  return `${index + 1}. ${game.title} (${game.provider})`;
+}
