e80239652715ea1d35688aa2dfbf7e17b159b02f
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-03-24 23:22:24 -0400

    Fix koth image attachment in Discord

--- src/commands/koth.js
+++ src/commands/koth.js
@@ -1,15 +1,32 @@
+const { AttachmentBuilder } = require("discord.js");
+
 module.exports = () => ({
   name: "koth",
   description: "Display current King of the Hill information",
   handler: async (ctx) => {
     // const koth = api.get('public', 'koth');
 
+    if (ctx.platform !== "discord") {
+      return ctx.sendForm({
+        emoji: "👑",
+        title: "KING OF THE HILL",
+        banner: "https://stats.chips.gg/koth",
+        buttonLabel: "BE KING.",
+        url: "https://chips.gg/koth",
+      });
+    }
+
     return ctx.sendForm({
       emoji: "👑",
       title: "KING OF THE HILL",
-      banner: `https://stats.chips.gg/koth`,
+      banner: `attachment://koth.png`,
       buttonLabel: "BE KING.",
       url: "https://chips.gg/koth",
+      files: [
+        new AttachmentBuilder("https://stats.chips.gg/koth", {
+          name: "koth.png",
+        }),
+      ],
     });
   },
 });
